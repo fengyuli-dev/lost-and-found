c051954ae17bf645cbb5d626df6495190b7d6ded
PORRIDGE-ZOU <89502566+PORRIDGE-ZOU@users.noreply.github.com>
2021-11-21 17:35:31 -0500

    PLEASE READ MY MESSAGE ON THE MODIFICATION OF THE CLASS FOUNDVIEWCONTROLLER.
    PLEASE READ MY MESSAGE ON THE MODIFICATION OF THE CLASS FOUNDVIEWCONTROLLER.

--- frontend/Lost and Found/L5Starter/FoundTableViewCell.swift
+++ frontend/Lost and Found/L5Starter/FoundTableViewCell.swift
@@ -4,10 +4,9 @@
 //
 //  Created by Vivian on 11/20/21.
 //
-
 import SwiftUI
 
-class FoundTableViewCell: UITableViewCell {
+class FoundTableViewCell: UICollectionViewCell {
     
     var name = UILabel()
     var location = UILabel()
@@ -15,11 +14,33 @@ class FoundTableViewCell: UITableViewCell {
     var des_image = UIImageView()
     var rec = UILabel()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(for item: Item) {
+        name.text = item.objectName
+        location.text = item.location
+        time.text = item.time
+        des_image.image = item.pics
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 25;
         
-        name.font = UIFont(name: "RoundedMplus1c-ExtraBold", size: 18)
+        
+        rec.layer.backgroundColor = UIColor(red: 0.941, green: 0.961, blue: 0.976, alpha: 1).cgColor
+        rec.layer.cornerRadius = 25
+        contentView.addSubview(rec)
+        
+        
+        name.font = UIFont(name: "RoundedMplus1c-ExtraBold", size: 50)
         name.translatesAutoresizingMaskIntoConstraints = false
+        name.textColor = .black;
+        name.adjustsFontSizeToFitWidth = true;
+        name.adjustsFontForContentSizeCategory = true;
         contentView.addSubview(name)
         
         location.font = UIFont(name: "RoundedMplus1c-Regular", size: 15)
@@ -27,74 +48,59 @@ class FoundTableViewCell: UITableViewCell {
         location.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(location)
         
-        time.font = UIFont(name: "RoundedMplus1c-Light", size: 10)
+        time.font = UIFont(name: "RoundedMplus1c-Light", size: 5)
         time.textColor = UIColor(red: 0.557, green: 0.557, blue: 0.557, alpha: 1)
+        time.adjustsFontSizeToFitWidth = true;
         time.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(time)
         
-        des_image.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(des_image)
-        
-        rec.frame = CGRect(x: 0, y: 0, width: 309, height: 92)
-        rec.layer.backgroundColor = UIColor(red: 0.941, green: 0.961, blue: 0.976, alpha: 1).cgColor
-        rec.layer.cornerRadius = 25
+//        des_image.translatesAutoresizingMaskIntoConstraints = false
+//        contentView.addSubview(des_image)
+        //haven't figured out how to handle images
         
         setupConstraints()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configure(for item: Found_Item) {
-        name.text = item.objectName
-        location.text = item.location
-        time.text = item.time
-        des_image.image = item.pics
-    }
     
     func setupConstraints() {
-//        let padding: CGFloat = 50
-//        let labelHeight: CGFloat = 20
-//
-        NSLayoutConstraint.activate([
-            rec.widthAnchor.constraint(equalToConstant: 309),
-            rec.heightAnchor.constraint(equalToConstant: 92),
-            rec.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            rec.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 164),
-        ])
         
-        
-        NSLayoutConstraint.activate([
-            name.widthAnchor.constraint(equalToConstant: 165),
-            name.heightAnchor.constraint(equalToConstant: 25),
-            name.leadingAnchor.constraint(equalTo: rec.leadingAnchor, constant: 21),
-            name.topAnchor.constraint(equalTo: location.topAnchor, constant: 10),
-        ])
+//        NSLayoutConstraint.activate([
+//            rec.widthAnchor.constraint(equalToConstant: 309),
+//            rec.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: CGFloat(0.75)),
+//            rec.heightAnchor.constraint(equalToConstant: 92),
+//            rec.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+//            rec.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+//        ])
         
         NSLayoutConstraint.activate([
             location.widthAnchor.constraint(equalToConstant: 180),
             location.heightAnchor.constraint(equalToConstant: 20),
-            location.leadingAnchor.constraint(equalTo: rec.leadingAnchor, constant: 21),
-            location.topAnchor.constraint(equalTo: rec.topAnchor, constant: 38),
+            location.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 21),
+            location.topAnchor.constraint(equalTo: name.topAnchor, constant: 28),
         ])
         
         NSLayoutConstraint.activate([
-            time.widthAnchor.constraint(equalToConstant: 35),
-            time.heightAnchor.constraint(equalToConstant: 20),
-            time.leadingAnchor.constraint(equalTo: rec.leadingAnchor, constant: 141),
-            time.topAnchor.constraint(equalTo: rec.topAnchor, constant: 15),
+            name.widthAnchor.constraint(equalToConstant: 130),
+            name.heightAnchor.constraint(equalToConstant: 50),
+            name.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 21),
+            name.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
         ])
         
+
         NSLayoutConstraint.activate([
-            des_image.widthAnchor.constraint(equalToConstant: 58),
-            des_image.heightAnchor.constraint(equalToConstant: 70),
-            des_image.leadingAnchor.constraint(equalTo: rec.leadingAnchor, constant: 229),
-            des_image.topAnchor.constraint(equalTo: rec.topAnchor, constant: 11),
+//            time.widthAnchor.constraint(equalToConstant: 35),
+            time.heightAnchor.constraint(equalToConstant: 15),
+            time.leadingAnchor.constraint(equalTo: name.trailingAnchor, constant: 20),
+            time.centerYAnchor.constraint(equalTo: name.centerYAnchor),
+            time.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
         ])
         
-        
-        
+//        NSLayoutConstraint.activate([
+//            des_image.widthAnchor.constraint(equalToConstant: 58),
+//            des_image.heightAnchor.constraint(equalToConstant: 70),
+//            des_image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 229),
+//            des_image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 11),
+//        ])
         
         
     }
