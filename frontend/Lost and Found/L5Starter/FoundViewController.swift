cea985a49268543740b06a1d4eaccac0ff57128f
PORRIDGE-ZOU <89502566+PORRIDGE-ZOU@users.noreply.github.com>
2021-11-21 17:33:58 -0500

    Modified the Constraints
    IMPORTANT: 我们应该用百分比来控制大小而不是绝对大小，不然手机型号不同展示出来的就会不太好看。我已经根据数据调整了每一个cell的大小（详见foundviewcontroller的倒数第二个function，就是我在算百分比，基本就是这个数据），
    但是，每个cell里面的每个label也应该用百分比控制大小。我还没有修改。如果你有时间可以修改一下。
    现在问题是：
    我们的字体大小好像不管用？？详见tableview cell的字体数据，我已经试了很多了，得出的结果还是一样的。为啥会这样？

--- frontend/Lost and Found/L5Starter/FoundViewController.swift
+++ frontend/Lost and Found/L5Starter/FoundViewController.swift
@@ -2,91 +2,125 @@
 //  ViewController.swift
 //  L5Starter
 //
-//  Created by Amy Chin Siu Huang on 10/25/21.
+//  Created by Vivian Fan on 11/20/2021.
 //
-
 import UIKit
 
-class FoundViewController: UIViewController, UITableViewDelegate {
+class FoundViewController: UIViewController{
     
-    // TODO 1: set up view
-    var foundTableView = UITableView()
+    var foundTableView : UICollectionView;
     let reuseIdentifier_1 = "foundCellReuse"
     
-    var foundItems: [Found_Item]=[]
+    var foundItems: [Item]=[]
     var PostLost_Button = UIButton()
+    var cellPadding : CGFloat = 10
 //    var recButton = UILabel()
-
-
+    
+    init(){
+        let foundlayout = UICollectionViewFlowLayout();
+        foundTableView = UICollectionView(frame: .zero, collectionViewLayout: foundlayout)//this is of no use.
+        super.init(nibName: nil, bundle: nil);
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Found Items"
-        let color: UIColor=UIColor(red: 0.788, green: 0.839, blue: 0.875, alpha: 1)
-        let textAttributes = [NSAttributedString.Key.foregroundColor: color]
-        navigationController?.navigationBar.titleTextAttributes = textAttributes
         view.backgroundColor = UIColor(red: 0.325, green: 0.38, blue: 0.424, alpha: 1)
         
+        
+        
+        let sectionpadding : CGFloat = 5;
+        let foundlayout = UICollectionViewFlowLayout();
+        foundTableView = UICollectionView(frame: .zero, collectionViewLayout: foundlayout)
         foundTableView.translatesAutoresizingMaskIntoConstraints = false
         foundTableView.dataSource = self
         foundTableView.delegate = self
-        foundTableView.register(FoundTableViewCell.self, forCellReuseIdentifier: reuseIdentifier_1)
+        foundTableView.backgroundColor = view.backgroundColor;
+        foundTableView.register(FoundTableViewCell.self, forCellWithReuseIdentifier: reuseIdentifier_1)
+        foundlayout.minimumLineSpacing = cellPadding;
+        foundlayout.scrollDirection = .vertical;
         view.addSubview(foundTableView)
+        
         
         PostLost_Button.frame = CGRect(x: 0, y: 0, width: 130, height: 46)
         PostLost_Button.setTitle("Post Lost", for: .normal)
         PostLost_Button.layer.backgroundColor = UIColor(red: 0.788, green: 0.839, blue: 0.875, alpha: 1).cgColor
         PostLost_Button.layer.cornerRadius = 23
         PostLost_Button.addTarget(self, action: #selector(PostLostTapped), for: .touchUpInside)
-        PostLost_Button.setTitleColor(.black, for: .normal)
         PostLost_Button.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(PostLost_Button)
         
+        
+        
         setupConstraints()
+        addDUMMYData();
     }
     
     
 
     func setupConstraints() {
-//        let collectionViewPadding: CGFloat = 12
+        let viewpadding : CGFloat = 10;
         NSLayoutConstraint.activate([
-            foundTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            foundTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            foundTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            foundTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            foundTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: viewpadding),
+            foundTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -viewpadding),
+            foundTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: viewpadding),
+            foundTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -viewpadding)
         ])
         
         NSLayoutConstraint.activate([
             PostLost_Button.widthAnchor.constraint(equalToConstant: 130),
             PostLost_Button.heightAnchor.constraint(equalToConstant: 46),
             PostLost_Button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            PostLost_Button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            PostLost_Button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
         ])
     }
     
+    
     @objc func PostLostTapped() {
         let PLVC = PostLostViewController();
-            self.navigationController?.pushViewController(PLVC, animated: true)
+        self.navigationController?.pushViewController(PLVC, animated: true)
     }
-}
-
-extension FoundViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foundItems.count
-        
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return FoundTableViewCell()
+    
+    
+    func addDUMMYData(){
+        foundItems.append(Item(objectName: "Porridge", location: "Townhouse", time: "25:61", note: "Handsome BOOOYYYY", contact: "Oopsy, you should find it", pics: UIImage()))
     }
 }
 
 
+extension FoundViewController : UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return foundItems.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = foundItems[indexPath.item];
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier_1, for: indexPath) as! FoundTableViewCell;
+        cell.configure(for: item);
+        return cell;
+    }
+    
+    
+}
 
 
-//extension ViewController: UITableViewDelegate {
-//
-//}
-
-// TODO 6: Conform to UICollectionViewDataSource
-
-// TODO 7: Confrom to UICollectionViewDelegateFlowLayout
-// TODO 9: Conform to UICollectionViewDelegate, implement interaction
+extension FoundViewController : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let theWidth = collectionView.frame.width * 0.825 ;
+        let theHeight = collectionView.frame.height * 0.125//note here.
+        return CGSize(width: theWidth, height: theHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = foundItems[indexPath.item];
+        let display = FoundDescViewController();
+        display.configure(for: item);
+        present(display, animated: true, completion: nil);
+    }
+    
+}
