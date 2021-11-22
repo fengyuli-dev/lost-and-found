32123af2ae850d8956527375629854f7a065895f
PORRIDGE-ZOU <89502566+PORRIDGE-ZOU@users.noreply.github.com>
2021-11-21 23:48:04 -0500

    Adjusted all the constraints.Continue TO READ THIS
    I have adjusted the constraints from absolute values into relative ones. HOWEVER, I have adjusted most of the display. NOT PRECISELY ACCORDING TO THE FIGMA. I have actually made a lot of adjustments to it. Because I simply could not figure out how to achieve that. The question is mainly that, when I transform the absolute values  of the fonts from Figma into relative ones, the result seems to be improper. As can be read from my code, I actually manually set all the proportion...
    Moreover, THERE ARE PROBLEMS REGARDING THE TIME. how can we set the time?

--- frontend/Lost and Found/L5Starter/FoundTableViewCell.swift
+++ frontend/Lost and Found/L5Starter/FoundTableViewCell.swift
@@ -25,32 +25,32 @@ class FoundTableViewCell: UICollectionViewCell {
     func configure(for item: Item) {
         name.text = item.objectName
         location.text = item.location
-        time.text = item.time
+        time.text = "\(item.time)"
         des_image.image = item.pics
         contentView.backgroundColor = .white
         contentView.layer.cornerRadius = 25;
-        
         
         rec.layer.backgroundColor = UIColor(red: 0.941, green: 0.961, blue: 0.976, alpha: 1).cgColor
         rec.layer.cornerRadius = 25
         contentView.addSubview(rec)
         
-        
-        name.font = UIFont(name: "RoundedMplus1c-ExtraBold", size: 50)
+        let namefont = contentView.frame.height * 0.25
+        name.font = UIFont(name: "RoundedMplus1c-ExtraBold", size: namefont)
+//        name.font = .systemFont(ofSize: 25)
         name.translatesAutoresizingMaskIntoConstraints = false
         name.textColor = .black;
-        name.adjustsFontSizeToFitWidth = true;
-        name.adjustsFontForContentSizeCategory = true;
         contentView.addSubview(name)
         
-        location.font = UIFont(name: "RoundedMplus1c-Regular", size: 15)
+        let locafont = namefont * (2/3)
+        location.font = UIFont(name:"RoundedMplus1c-Regular", size: locafont)
         location.textColor=UIColor(red: 0.557, green: 0.557, blue: 0.557, alpha: 1)
         location.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(location)
         
-        time.font = UIFont(name: "RoundedMplus1c-Light", size: 5)
+        //do we really need this?
+        let timefont = locafont * (5/6)
+        time.font = UIFont(name: "RoundedMplus1c-Light", size: timefont)
         time.textColor = UIColor(red: 0.557, green: 0.557, blue: 0.557, alpha: 1)
-        time.adjustsFontSizeToFitWidth = true;
         time.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(time)
         
@@ -64,45 +64,34 @@ class FoundTableViewCell: UICollectionViewCell {
     
     func setupConstraints() {
         
-//        NSLayoutConstraint.activate([
-//            rec.widthAnchor.constraint(equalToConstant: 309),
-//            rec.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: CGFloat(0.75)),
-//            rec.heightAnchor.constraint(equalToConstant: 92),
-//            rec.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-//            rec.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
-//        ])
-        
+        print("contentview: \(contentView.frame.height), wide\(contentView.frame.width)")
+        let toppad2 = contentView.frame.height * 0.108
+        let nameheight = contentView.frame.height * 0.35
+        let namelead = contentView.frame.width * 0.08
+        let namewidth = contentView.frame.width * (3/7)
+        print("\(toppad2),\(nameheight),\(namelead)")
         NSLayoutConstraint.activate([
-            location.widthAnchor.constraint(equalToConstant: 180),
-            location.heightAnchor.constraint(equalToConstant: 20),
-            location.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 21),
-            location.topAnchor.constraint(equalTo: name.topAnchor, constant: 28),
+            name.widthAnchor.constraint(equalToConstant: namewidth),
+            name.heightAnchor.constraint(equalToConstant: nameheight),
+            name.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: namelead),
+            name.topAnchor.constraint(equalTo: contentView.topAnchor, constant: toppad2),
         ])
         
+        
         NSLayoutConstraint.activate([
-            name.widthAnchor.constraint(equalToConstant: 130),
-            name.heightAnchor.constraint(equalToConstant: 50),
-            name.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 21),
-            name.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            location.leadingAnchor.constraint(equalTo: name.leadingAnchor),
+            location.topAnchor.constraint(equalTo: name.bottomAnchor),
+            location.widthAnchor.constraint(equalTo: name.widthAnchor)
+        ])
+        
+        
+        NSLayoutConstraint.activate([
+            time.topAnchor.constraint(equalTo: location.bottomAnchor, constant: toppad1),
+            time.leadingAnchor.constraint(equalTo: name.leadingAnchor),
+            time.widthAnchor.constraint(equalTo: name.widthAnchor)
         ])
         
 
-        NSLayoutConstraint.activate([
-//            time.widthAnchor.constraint(equalToConstant: 35),
-            time.heightAnchor.constraint(equalToConstant: 15),
-            time.leadingAnchor.constraint(equalTo: name.trailingAnchor, constant: 20),
-            time.centerYAnchor.constraint(equalTo: name.centerYAnchor),
-            time.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-        ])
-        
-//        NSLayoutConstraint.activate([
-//            des_image.widthAnchor.constraint(equalToConstant: 58),
-//            des_image.heightAnchor.constraint(equalToConstant: 70),
-//            des_image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 229),
-//            des_image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 11),
-//        ])
-        
-        
     }
 
 }
