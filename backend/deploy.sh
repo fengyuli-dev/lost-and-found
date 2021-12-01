b81bc5c252a5de5ca98550cd1e5244dc443d3f94
Li Fengyu <fengyuli2002@gmail.com>
2021-11-30 23:31:50 -0500

    Update deploy shell script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,6 +2,7 @@
 
 docker buildx build --platform linux/amd64 --push -t registry.heroku.com/fengyulilostandfound/web .
 heroku container:release web --app fengyulilostandfound
-# docker push fengyuli2002/lost_and_found
+docker tag registry.heroku.com/fengyulilostandfound/web fengyuli2002/lost_and_found
+docker push fengyuli2002/lost_and_found
 
 echo "Deployed a new version of lost_and_found"
