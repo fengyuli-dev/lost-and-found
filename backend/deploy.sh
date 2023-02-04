f872ed5490e3d38191b5ba4a509d75e97334223e
Fengyu Li <fengyuli2002@gmail.com>
2023-02-04 12:36:16 -0500

    Remove all docker stuff

--- backend/deploy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-
-docker buildx build --platform linux/amd64 --push -t registry.heroku.com/fengyulilostandfound/web .
-heroku container:release web --app fengyulilostandfound
-docker tag registry.heroku.com/fengyulilostandfound/web fengyuli2002/lost_and_found
-docker push fengyuli2002/lost_and_found
-
-echo "Deployed a new version of lost_and_found"
